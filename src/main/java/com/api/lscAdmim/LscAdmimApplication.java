56e8c33191a84a13f16d42ffd6b231ebb5faeccd
cfl.dev.apps <cfl.dev.apps@gmail.com>
2025-02-09 13:11:07 +0000

    initial project

---
+++ src/main/java/com/api/lscAdmim/LscAdmimApplication.java
@@ -0,0 +1,13 @@
+package com.api.lscAdmim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LscAdmimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LscAdmimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/lscAdmim/LscAdmimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.lscAdmim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LscAdmimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
