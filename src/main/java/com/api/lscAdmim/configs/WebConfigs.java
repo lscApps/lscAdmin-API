76621c0ed2c64508a203e15f2f378be70e16570b
cfl.dev.apps <cfl.dev.apps@gmail.com>
2025-03-15 12:42:09 +0000

    add origins restrictions

--- src/main/java/com/api/lscAdmim/configs/WebConfigs.java
+++ src/main/java/com/api/lscAdmim/configs/WebConfigs.java
@@ -1,5 +1,6 @@
 package com.api.lscAdmim.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,16 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfigs {
-	
+
+	@Value("${lscAdmin.allowed.origins}")
+	private String alloweOrigins;
+
 	@Bean
-    public WebMvcConfigurer corsConfigurer()
-    {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-            }
-        };
-    }
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(alloweOrigins).allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
 
 }
