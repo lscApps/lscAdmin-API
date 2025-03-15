caccbd948c7b6952382b13a4d32151500d57e654
lscApps <lscfinancial25+github@gmail.com>
2025-03-15 15:07:22 +0000

    add security features (#10)
    * add origins restrictions
    
    * add headers to force CORS configs
    
    * add logs to show allowed Origins
    
    ---------
    
    Co-authored-by: cfl.dev.apps <cfl.dev.apps@gmail.com>

--- src/main/java/com/api/lscAdmim/configs/WebConfigs.java
+++ src/main/java/com/api/lscAdmim/configs/WebConfigs.java
@@ -6,18 +6,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class WebConfigs {
 
 	@Value("${lscAdmin.allowed.origins}")
-	private String alloweOrigins;
+	private String allowedOrigins;
 
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
+		
+		log.info(String.format("AllowedOrigins = %s", allowedOrigins));
+		
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(alloweOrigins).allowedMethods("GET", "POST", "PUT", "DELETE");
+				registry.addMapping("/**").allowedOrigins(allowedOrigins).allowedMethods("GET", "POST", "PUT", "DELETE");
 			}
 		};
 	}
