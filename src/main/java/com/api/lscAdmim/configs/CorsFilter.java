212edd734017420bf294fa40579d70eb26f744dd
cfl.dev.apps <cfl.dev.apps@gmail.com>
2025-03-15 15:03:14 +0000

    add headers to force CORS configs

---
+++ src/main/java/com/api/lscAdmim/configs/CorsFilter.java
@@ -0,0 +1,39 @@
+package com.api.lscAdmim.configs;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CorsFilter implements Filter {
+	
+	@Value("${lscAdmin.allowed.origins}")
+	private String alloweOrigins;
+	
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", alloweOrigins);
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        response.setHeader("Vary", "Origin");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
+
