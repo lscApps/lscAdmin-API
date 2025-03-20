de97ed8f47696fefe2e1787ee7a26fbae8e360fa
cfl.dev.apps <cfl.dev.apps@gmail.com>
2025-03-20 00:40:43 +0000

    removing unecessary imports

--- src/main/java/com/api/lscAdmim/configs/WebConfigs.java
+++ src/main/java/com/api/lscAdmim/configs/WebConfigs.java
@@ -1,16 +1,12 @@
 package com.api.lscAdmim.configs;
 
+import com.api.lscAdmim.filters.CorsFilter;
+import com.api.lscAdmim.filters.JwtValidationFilter;
+import jakarta.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.api.lscAdmim.filters.CorsFilter;
-import com.api.lscAdmim.filters.JwtValidationFilter;
-
-import jakarta.servlet.Filter;
 
 @Configuration
 public class WebConfigs {
