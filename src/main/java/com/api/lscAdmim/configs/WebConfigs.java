0e047ced585f7bf3ebf4749db85f1b5fe3e77a58
lscApps <lscfinancial25+github@gmail.com>
2025-03-20 00:42:19 +0000

    bugfix (#14)
    * removing CorsFilter
    
    * add token generation
    
    * add CORS filter and origins control
    
    * code improvements and report bugfix
    
    * bugfix jwtValidationFilter register
    
    * removing unecessary imports
    
    ---------
    
    Co-authored-by: cfl.dev.apps <cfl.dev.apps@gmail.com>

--- src/main/java/com/api/lscAdmim/configs/WebConfigs.java
+++ src/main/java/com/api/lscAdmim/configs/WebConfigs.java
@@ -1,18 +1,18 @@
 package com.api.lscAdmim.configs;
 
+import com.api.lscAdmim.filters.CorsFilter;
+import com.api.lscAdmim.filters.JwtValidationFilter;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.api.lscAdmim.filters.CorsFilter;
-import com.api.lscAdmim.filters.JwtValidationFilter;
-
-import jakarta.servlet.Filter;
 
 @Configuration
 public class WebConfigs {
+
+    @Autowired
+    private JwtValidationFilter jwtValidationFilter;
 
 	@Bean
     public FilterRegistrationBean<Filter> customCorsFilter() {
@@ -26,10 +26,12 @@ public class WebConfigs {
 	@Bean
     public FilterRegistrationBean<Filter> jwtFilter() {
         FilterRegistrationBean<Filter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtValidationFilter());
+        registrationBean.setFilter(jwtValidationFilter);
         registrationBean.addUrlPatterns("/lscAdmin/api/*");
         registrationBean.setOrder(2);
         return registrationBean;
     }
+
+
 
 }
